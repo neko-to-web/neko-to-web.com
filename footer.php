b7b442b46320700095ebc6adffbe228a81724f0e
Ayumi Yamaguchi <a.yamaguchi@to-mode.com>
2015-10-30 19:53:15 +0900

    必要最低限のファイル追加

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer id="footer">
+  <p class="copyright"><?php echo date('Y'); ?> neko-to-web.com</p>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,25 @@
+<html prefix="og: http://ogp.me/ns#">
+<head>
+<meta charset="UTF-8">
+<title></title>
+<meta name="viewport" content="width=device-width">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+<?php wp_head(); ?>
+</head>
+
+<body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46464800-9', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<header id="header">
+  <h1><a href="/">猫とWeb</a></h1>
+</header>
